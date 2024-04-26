e3bf911f32dda2c4bb762c0b410827cfd2ba91f7
aka-radix <h.radiahmad@gmail.com>
2024-04-26 14:17:21 +0300

    chore: set up base configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+      formats: ["image/avif", "image/webp"],
+      remotePatterns: [
+        {
+          protocol: "http",
+          hostname: "127.0.0.1",
+          port: "8000",
+          pathname: "/media/items/*"
+        }
+      ]
+    }
+};
 
 export default nextConfig;
