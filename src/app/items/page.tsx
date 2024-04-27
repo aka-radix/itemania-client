c59c05f8aa8b24cbd11954f041f27b73e75a71d5
aka-radix <h.radiahmad@gmail.com>
2024-04-27 16:10:01 +0300

    fix: force dynamic behavior

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -2,6 +2,9 @@ import styles from "@/app/page.module.css"
 import { listItems } from "@/utils/data"
 import { ItemCard } from "@/components/ui/item-card"
 
+export const dynamic = "force-dynamic"
+export const revalidate = 1
+
 export default async function Page() {
   const items = await listItems()
 
